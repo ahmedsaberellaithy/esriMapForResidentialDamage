c994a4bdfd861ac737055b7995e2e05f7f0f0059
Ahmed Saber <A.SABER@eseed.net>
2021-02-03 15:15:52 +0200

    readme updated

--- js/mapInit.js
+++ js/mapInit.js
@@ -1,7 +1,13 @@
 //reuseable public functions
-let makeAbuffer,showBuffer;
+let makeAbuffer,showBuffer,executeQuery;
 //map instance
-let map,currPoint,gsvc,search,dialog;
+let map,currPoint,gsvc,queryTask;
+
+//useful variables
+const pointsHashmap = {};
+let pointsCounter = 0;
+const layerUrl = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessmentStatePlane/MapServer/0";
+
 //required features from ARCGIS js 3.35
 const requiredFeatures = [
   "esri/map",
@@ -9,21 +15,20 @@ const requiredFeatures = [
   "esri/geometry/webMercatorUtils",
   "esri/tasks/GeometryService",
   "esri/tasks/BufferParameters",
-  "esri/dijit/Search",
   "esri/InfoTemplate",
-  "esri/symbols/SimpleFillSymbol",
-  "esri/symbols/SimpleLineSymbol",
-  "esri/renderers/SimpleRenderer",
-  "esri/graphic",
-  "esri/lang",
-  "esri/Color",
-  "dojo/number",
-  "dojo/dom-style",
-  "dijit/TooltipDialog",
-  "dijit/popup",
+  "esri/tasks/query",
+  "esri/tasks/QueryTask",
+  "esri/symbols/SimpleMarkerSymbol",
   "dojo/dom",
   "dojo/domReady!"
 ];
+
+function getFormattedInfoTemplateContent(graphic){
+  const attributes = graphic.attributes;
+  const dateFormatted = new Date(attributes.inspdate);
+
+  return `Contact First Name: ${attributes.firstname}</br>Incident Name: ${attributes.incidentnm}</br>Inspection DateTime: ${dateFormatted}`;
+}
 
 require(requiredFeatures,
 function(
@@ -32,18 +37,10 @@ function(
   webMercatorUtils,
   GeometryService,
   BufferParameters,
-  Search,
   InfoTemplate,
-  SimpleFillSymbol,
-  SimpleLineSymbol,
-  SimpleRenderer,
-  Graphic,
-  esriLang,
-  Color,
-  number,
-  domStyle,
-  TooltipDialog,
-  dijitPopup,
+  Query,
+  QueryTask,
+  SimpleMarkerSymbol,
   dom) {
 
   //initialize the map, and pass the div where it should go
@@ -53,102 +50,102 @@ function(
     // longitude, latitude
     // cairo center (31.2357,30.0444)
     center: [-88.2, 41.8],
-    zoom: 5
+    zoom: 8
   });
+
+  const infoTemplate = new InfoTemplate(
+    "Entity with ObjectId ${objectid}",
+    getFormattedInfoTemplateContent);
 
   //define an instance for feature layer, then add the layer on the map
   const damageAssessmentStatePlane = new FeatureLayer(
-      "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessmentStatePlane/MapServer/0", {
-      outFields: ["*"]
+      layerUrl, {
+      outFields: ["objectid","firstname","incidentnm","inspdate"],
+      infoTemplate
     });
-
-  //make custom renderer before adding the layer
-  // const symbol = new SimpleFillSymbol(
-  //   SimpleFillSymbol.STYLE_SOLID,
-  //   new SimpleLineSymbol(
-  //     SimpleLineSymbol.STYLE_SOLID,
-  //     new Color([255,255,255,0.35]),
-  //     1
-  //   ),
-  //   new Color([125,125,125,0.35])
-  // );
-  // damageAssessmentStatePlane.setRenderer(new SimpleRenderer(symbol));
 
   map.addLayer(damageAssessmentStatePlane);
-  map.infoWindow.resize(245,125);
 
-  const highlightSymbol = new SimpleFillSymbol(
-    SimpleFillSymbol.STYLE_SOLID,
-    new SimpleLineSymbol(
-      SimpleLineSymbol.STYLE_SOLID,
-      new Color([255,0,0]), 3
-    ),
-    new Color([125,125,125,0.35])
-  );
+  //initialize query task
+  queryTask = new QueryTask(layerUrl);
 
-  //listen for when the onMouseOver event fires on the feature layer added
-  //when fired, create a new graphic with the geometry from the event.graphic and add it to the maps graphics layer
-  damageAssessmentStatePlane.on("mouse-over", function(evt){
-    const text = "Entity with ObjectId ${objectid}"+
-        "Contact First Name: ${firstname}</br>Incident Name: ${incidentnm}</br>Inspection Date Time: ${inspdate}</br>Thank you!";
 
-    const content = esriLang.substitute(evt.graphic.attributes,text);
-    const highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);
-    map.graphics.add(highlightGraphic);
+  //that was initially used with scope change it was planned to be used with the listing
+  map.on('extent-change', function (evt) {
+    query.geometry = evt.extent;
+    damageAssessmentStatePlane.selectFeatures(query, FeatureLayer.SELECTION_NEW, function (features) {
+      dom.byId("count").innerHTML = features.length;
+      let results = ""
+        resultsHeader = "<tr><th>ObjectId</th>";
+      
+      resultsHeader += "<th>first Name</th>";
+      resultsHeader += "<th>incident Name</th>";
+      resultsHeader += "<th>inspection Date</th>";
+      resultsHeader += "<th>Show Point</th></tr>";
 
-    dialog.setContent(content);
-
-    domStyle.set(dialog.domNode, "opacity", 0.85);
-    dijitPopup.open({
-      popup: dialog,
-      x: evt.pageX,
-      y: evt.pageY
+      for (var i = 0; i < features.length; i++) {
+        if(features[i].attributes.objectid){
+          pointId = pointsCounter++;
+          pointsHashmap[pointId]=features[i].geometry;
+          results += "<tr><td>" + features[i].attributes.objectid +"</td>";
+          results += "<td>" + features[i].attributes.firstname +"</td>";
+          results += "<td>" + features[i].attributes.incidentnm +"</td>";
+          results += "<td>" + new Date(features[i].attributes.inspdate) +"</td>";
+          results += "<td>" + `<button type="button" class="btn btn-sm btn-success" id="bufferButton" onclick="makeAbuffer(${pointId})">buffer</button>` +"</td>";
+          results += "</td></tr>";
+        }
+      }
+      dom.byId("resultsTableHead").innerHTML = resultsHeader;
+      dom.byId("resultsTableBody").innerHTML = results;
     });
   });
 
-  //info dialog initialization
-  dialog = new TooltipDialog({
-    id: "tooltipDialog",
-    style: "position: absolute; width: 250px; font: normal normal normal 10pt Helvetica;z-index:100"
-  });
-  dialog.startup();
 
-  //define new search instance & search widget
-  search = new Search({
-    map: map,
-    sources: [],
-    zoomScale: 5000000
-  }, "search");
+  //initialize query
+  query = new Query();
+  query.returnGeometry = true;
+  query.outFields = ["objectid","firstname","incidentnm","inspdate"];
 
-  const sources = search.sources;
-  sources.push({
-    featureLayer: damageAssessmentStatePlane,
-    placeholder: "Search ObjectId",
-    enableLabel: false,
-    searchFields: ["objectId"],
-    displayField: "Object Id",
-    exactMatch: false,
-    outFields: ["*"],
-    infoTemplate: new InfoTemplate("Entity with ObjectId ${objectid}",
-    "Contact First Name: ${firstname}</br>Incident Name: ${incidentnm}</br>Inspection Date Time: ${inspdate}</br>Thank you!")
-
-  });
-  //Set the sources above to the search widget
-  search.set("sources", sources);
-  search.startup();
+  //create symbol for selected features
+  var symbol = new SimpleMarkerSymbol();
+  symbol.setStyle(SimpleMarkerSymbol.STYLE_SQUARE);
+  symbol.setSize(10);
 
   map.on("load", function() {
     //after map loads, connect to listen to mouse move & drag events
     map.on("mouse-move", showCoordinates);
     map.on("mouse-drag", showCoordinates);
-    //update mouse event for modal
-    map.graphics.enableMouseEvents();
-    map.graphics.on("mouse-out", closeDialog);
   });
 
-  function closeDialog() {
+  executeQuery = () => {
+    const searchValue = dom.byId("objectIdSearchValue").value ;
+    //set query based on what user typed in for population;
+    query.where = "objectid > " + searchValue;
+    //execute query
+    queryTask.execute(query,showResults);
+  }
+
+  function showResults(featureSet){
+    //remove all graphics on the maps graphics layer
     map.graphics.clear();
-    dijitPopup.close(dialog);
+
+    //Performance enhancer - assign featureSet array to a single variable.
+    var resultFeatures = featureSet.features;
+
+    //Loop through each feature returned
+    for (var i=0, il=resultFeatures.length; i<il; i++) {
+      //Get the current feature from the featureSet.
+      //Feature is a graphic
+      var graphic = resultFeatures[i];
+      console.log(resultFeatures);
+      graphic.setSymbol(symbol);
+
+      //Set the infoTemplate.
+      graphic.setInfoTemplate(infoTemplate);
+
+      //Add graphic to the map graphics layer.
+      map.graphics.add(graphic);
+    }
   }
 
   function showCoordinates(evt) {
@@ -159,13 +156,17 @@ function(
     dom.byId("coordinates").innerHTML = mp.x.toFixed(3) + ", " + mp.y.toFixed(3);
   }
   gsvc = new GeometryService("https://utility.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
-  makeAbuffer = ()=>{
-    console.log({
-      currPoint
-    });
+  makeAbuffer = (pointId)=>{
+    let point = null;
+    if(!pointId){
+      point = currPoint; //current point represents the last active point under the map
+    }else{
+      point = pointsHashmap[pointId];
+      currPoint = point;
+    }
     map.graphics.clear();
     var params = new BufferParameters();
-    params.geometries = [ currPoint ];
+    params.geometries = [ point ];
 
     //buffer in linear units such as meters, km, miles etc.
     params.distances = [ 2, 2 ];
@@ -180,7 +181,7 @@ function(
       esri.symbol.SimpleFillSymbol.STYLE_SOLID,
       new esri.symbol.SimpleLineSymbol(
         esri.symbol.SimpleLineSymbol.STYLE_SOLID,
-        new dojo.Color([0,0,255,0.65]), 2
+        new dojo.Color([128,128,128,0.65]), 2
       ),
       new dojo.Color([0,0,255,0.35])
     );
---
+++ readme.md
@@ -0,0 +1,13 @@
+### Search and filter application using arcGIS javascript 3.35
+
+Author Ahmed Saber Ahmed
+
+#### Target
+1. Showing a service layer on a map
+2. Adding Query to the map and filter by Object Ids
+3. Show query results using Kindo Grid
+
+#### Current Status and File Structure
+- Basic HTML intro page containing the active map + a bootstrap table planned to be filled with data info
+- Kindo was not implemented, for some reason cannot be properly imported (To be visited later)
+- Query Instance created to be used in filtering
